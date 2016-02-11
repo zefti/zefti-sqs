f7cde0fb674c8bd0832cb78d4f894fac8cfe3d54
RYAN OGLE <ryanogle@RYANs-MacBook-Air-2.local>
2016-02-11 01:24:25 -0800

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var AWS = require('aws-sdk');
 
-module.exports = function(config){
+module.exports = function(options){
+  var dataSource = options.dataSource;
   AWS.config.update({region: 'us-east-1'});
-  AWS.config.update({accessKeyId: config.accessKeyId, secretAccessKey: config.secretAccessKey});
+  AWS.config.update({accessKeyId: dataSource.accessKeyId, secretAccessKey: dataSource.secretAccessKey});
   var sqs = new AWS.SQS();
   return sqs;
 };
